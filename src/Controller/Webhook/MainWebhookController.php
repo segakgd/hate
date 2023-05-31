7ce95da47e2ffed7934325d64e79e5dae40f57c9
sega <hpsegeyy@yandex.ru>
2023-05-31 21:29:59 +0200

    Update MainWebhookController.php

--- src/Controller/Webhook/MainWebhookController.php
+++ src/Controller/Webhook/MainWebhookController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Webhook;
 
 use App\Entity\Action;
 use App\Repository\ActionRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,12 +13,28 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class MainWebhookController extends AbstractController
 {
+    private const USER_SETTING = [
+        'bot_command' => [
+            '/command1' => [
+                'type' => 'message',
+                'content' => 'бла бла бла ',
+            ],
+            '/command2' => [
+                'type' => 'message',
+                'content' => 'бла бла бла 2',
+            ]
+        ]
+    ];
+
     public function __construct(
         private readonly SerializerInterface $serializer,
         private readonly ActionRepository $actionRepository,
     ) {
     }
 
+    /**
+     * @throws Exception
+     */
     #[Route('/webhook/', name: 'app_webhook', methods: ['POST'])]
     public function addWebhookAction(Request $request): JsonResponse
     {
@@ -36,27 +53,55 @@ class MainWebhookController extends AbstractController
         return new JsonResponse();
     }
 
+    /**
+     * @throws Exception
+     */
     private function getType(array $arrayWebhookData): ?array // todo не место этой функции тут
     {
-        if (
-            isset($arrayWebhookData['message']['entities'][0]['type']) &&
-            $arrayWebhookData['message']['entities'][0]['type'] === 'bot_command'
-        ) {
-            return [
-                'chatId' => $arrayWebhookData['message']['chat']['id'],
-                'type' => 'command',
-                'content' => $arrayWebhookData['message']['text'],
-            ];
+        if ($this->isCommand($arrayWebhookData)) {
+            return $this->createCommandAction($arrayWebhookData);
         }
 
-        if (isset($arrayWebhookData['message']['text'])){
-            return [
-                'chatId' => $arrayWebhookData['message']['chat']['id'],
-                'type' => 'message',
-                'content' => $arrayWebhookData['message']['text'],
-            ];
+        if ($this->isMessage($arrayWebhookData)){
+            return $this->createMessageAction($arrayWebhookData);
         }
 
         return null;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function createCommandAction(array $arrayWebhookData): array // todo не место этой функции тут
+    {
+        $setting = self::USER_SETTING;
+
+        $action = $setting['bot_command'][$arrayWebhookData['message']['text']] ??
+            throw new Exception('Undefined command ' . $arrayWebhookData['message']['text'])
+        ;
+
+        $action['chatId'] = $arrayWebhookData['message']['chat']['id'];
+
+        return $action;
+    }
+
+    private function createMessageAction(array $arrayWebhookData): array // todo не место этой функции тут
+    {
+        return [
+            'chatId' => $arrayWebhookData['message']['chat']['id'],
+            'type' => 'message',
+            'content' => $arrayWebhookData['message']['text'],
+        ];
+    }
+
+    private function isCommand(array $arrayWebhookData): bool // todo не место этой функции тут
+    {
+        return isset($arrayWebhookData['message']['entities'][0]['type']) &&
+            $arrayWebhookData['message']['entities'][0]['type'] === 'bot_command';
+    }
+
+    private function isMessage(array $arrayWebhookData): bool // todo не место этой функции тут
+    {
+        return isset($arrayWebhookData['message']['text']);
     }
 }
