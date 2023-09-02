f296d267d0b8a6b292b418c2fc5a6e51711df93d
sega <hpsegeyy@yandex.ru>
2023-09-02 21:28:30 +0200

    Применил рефлексию для проверки объектов в тестах

--- tests/Unit/UnitTestCase.php
+++ tests/Unit/UnitTestCase.php
@@ -3,7 +3,27 @@
 namespace App\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use ReflectionObject;
 
 class UnitTestCase extends TestCase
 {
+    /**
+     * Проверяет соответствие данных свойств объекта с параметрами массива
+     *
+     * @throws ReflectionException
+     */
+    public function assertObjectVars(object $object, array $properties): void
+    {
+        // Используем рефлексию php чтоб прочитать даже private данные
+        $reflectionObject = new ReflectionObject($object);
+
+        foreach ($properties as $propertyName => $propertyValue){
+            // проверяем, есть ли заданное в параметрах свойство
+            $reflectionObject->hasProperty($propertyName);
+
+            // проверяем значение
+            self::assertEquals($reflectionObject->getProperty($propertyName)->getValue($object), $propertyValue);
+        }
+    }
 }
