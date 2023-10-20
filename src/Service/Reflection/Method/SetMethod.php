d65e3d99b56e82e880d8aabc2ba12527cbf65a11
sega <hpsegeyy@yandex.ru>
2023-10-20 19:22:01 +0200

    +

--- src/Service/Reflection/Method/SetMethod.php
+++ src/Service/Reflection/Method/SetMethod.php
@@ -7,12 +7,14 @@ class SetMethod implements MethodInterface
     public function __construct(private string $var, private array $arg = [])
     {}
 
-    public function apply(object $targetObject): void
+    public function apply(object $targetObject): mixed
     {
         $var = $this->var;
 
         if (!empty($this->arg[0])){
             $targetObject->$var = $this->arg[0];
         }
+
+        return $targetObject;
     }
 }
